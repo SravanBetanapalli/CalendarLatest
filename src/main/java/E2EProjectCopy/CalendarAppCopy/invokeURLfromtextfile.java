9564b31a64b3a7e37753387a78df3508378d70a3
akhilbingi <66700648+akhilbingi@users.noreply.github.com>
2021-04-15 19:09:20 -0400

    Update invokeURLfromtextfile.java

--- src/main/java/E2EProjectCopy/CalendarAppCopy/invokeURLfromtextfile.java
+++ src/main/java/E2EProjectCopy/CalendarAppCopy/invokeURLfromtextfile.java
@@ -13,14 +13,11 @@ public class invokeURLfromtextfile extends mainClass {
 
 	public String geturl() throws IOException {
 		// TODO Auto-generated method stub
-		System.out.println("The execution entered inside invokefrom url class");
 		FileInputStream fis = new FileInputStream(
 				System.getProperty("user.dir") + "/src/main/java/E2EProjectCopy/CalendarAppCopy/Data.properties");
 		prop = new Properties();
 		prop.load(fis);
-		
 		String websitename = prop.getProperty("url");
-		System.out.println("the website name is : "+ websitename);
 		return websitename;
 	}
 }
