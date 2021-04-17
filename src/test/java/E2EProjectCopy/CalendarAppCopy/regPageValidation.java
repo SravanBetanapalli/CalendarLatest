5e50b5b798ee71c369c33fca9cc79afb1ac396ba
akhilbingi <66700648+akhilbingi@users.noreply.github.com>
2021-04-17 16:41:15 -0400

    Update regPageValidation.java

--- src/test/java/E2EProjectCopy/CalendarAppCopy/regPageValidation.java
+++ src/test/java/E2EProjectCopy/CalendarAppCopy/regPageValidation.java
@@ -36,8 +36,10 @@ public class regPageValidation extends mainClass {
 		reg.getpassword().sendKeys(password);
 		reg.getAvatar().sendKeys(URL);
 		reg.Submit().click();
+		WebDriverWait wait = new WebDriverWait(driver, 30);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//p[@id='temperature']")));
 		// cust_url is the url of the login user at that instance
-		String cust_url = i.geturl() + "/calendar/" + Username;
+		String cust_url = i.geturl() + "calendar/" + Username;
 		System.out.println(cust_url);
 		String curl_url = driver.getCurrentUrl();
 		System.out.println(curl_url);
